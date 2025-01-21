49ab6d80c4b26ad14fbb9ea30cd87e874ae067fb
Paulo Borges <victorborges296@hotmail.com>
2025-01-20 21:06:15 -0300

    create-chatbot route added to the project

--- app/(admin)/create-chatbot/page.tsx
+++ app/(admin)/create-chatbot/page.tsx
@@ -1,8 +1,43 @@
+"use client"
+
 import Avatar from "@/components/Avatar";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { CREATE_CHATBOT } from "@/graphql/mutations";
+import { useMutation } from "@apollo/client";
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
 
 function CreateChatbot() {
+   const { user } = useUser();
+   const [name, setName] = useState("");
+   const router = useRouter();
+   const [createChatbot, { data, loading, error }] = useMutation(CREATE_CHATBOT, {
+      variables: {
+         clerk_user_id: user?.id,
+         name
+      }
+   });
+
+   async function handleSubmit(e: FormEvent) {
+      e.preventDefault();
+
+      try {
+         const data = await createChatbot();
+
+         setName("");
+
+         router.push(`/edit-chatbot/${data.data.insertChatbots.id}`);
+      } catch (error) {
+         console.log(error);
+      }
+   }
+
+   if (!user) {
+      return null;
+   }
+
    return (
       <div className="flex flex-col items-center justify-center md:flex-row md:space-x-10 bg-white p-10 rounded-md m-10">
          <Avatar seed="create-chatbot" />
@@ -11,15 +46,21 @@ function CreateChatbot() {
             <h2 className="font-light">
                Create a new chatbot to assist you in your conversations with your customers.
             </h2>
-            <form className="flex flex-col md:flex-row gap-2 mt-5">
+            <form onSubmit={handleSubmit} className="flex flex-col md:flex-row gap-2 mt-5">
                <Input
                   type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
                   placeholder="Chatbot Name..."
                   className="max-w-lg"
                   required
                />
-               <Button>Create Chatbot</Button>
+               <Button type="submit" disabled={loading || !name}>
+                  {loading ? "Creating Chatbot..." : "Create Chatbot"}
+               </Button>
             </form>
+
+            <p className="text-gray-300 mt-5">Example: Customer Support</p>
          </div>
       </div>
    )
